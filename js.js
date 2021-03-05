e0adbede46f2f8d7dedd45a5d68da75338e85a1c
Johnny Deymisson <68932001+jdeymisson@users.noreply.github.com>
2021-03-04 22:07:10 -0300

    Up files

---
+++ js.js
@@ -0,0 +1,6 @@
+function menu () {
+    document.getElementById("bar1").classList.toggle('bar1');
+    document.getElementById("bar2").classList.toggle('bar2');
+    document.getElementById("bar3").classList.toggle('bar3');
+    document.getElementById("menu-area").classList.toggle('menu-area');
+}
